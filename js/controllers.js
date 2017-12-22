45d3d48cb2f292951286225432812dafa77c91a6
chanryTW <xd9440206@gmail.com>
2017-12-22 14:42:20 +0800

    2

---
+++ js/controllers.js
@@ -0,0 +1,70 @@
+angular.module('app.controllers', [])
+  
+// ----------------------------------------登入頁面----------------------------------------
+.controller('page5Ctrl', ['$scope', '$stateParams', '$ionicPopup',
+function ($scope, $stateParams, $ionicPopup) {
+    // 登入
+    var accountL = document.getElementById("page5-input1");
+    var pwdL = document.getElementById("page5-input2");
+    var loginSmtBtn = document.getElementById("page5-button1");
+    loginSmtBtn.addEventListener("click",function(){
+        console.log(accountL.value);
+        firebase.auth().signInWithEmailAndPassword(accountL.value, pwdL.value).then(function(){
+            console.log("登入成功");
+            localStorage.setItem("LoginWay", "Signin"); // 登入方式標記為 首頁登入
+            accountL.value="";
+            pwdL.value="";
+            open("/#/page1/page2",'_self');
+            // window.location.reload();
+        }).catch(function(error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+            switch(errorCode){
+                case 'auth/user-not-found':
+                    var alertPopup = $ionicPopup.alert({
+                        title: '發生錯誤',
+                        template: '查無此帳號。'
+                    });
+                        alertPopup.then(function(res) {
+                        accountL.value="";
+                        pwdL.value="";
+                    });
+                    break;
+                case 'auth/invalid-email':
+                    var alertPopup = $ionicPopup.alert({
+                        title: '發生錯誤',
+                        template: '電子信箱的格式有誤。'
+                    });
+                        alertPopup.then(function(res) {
+                        accountL.value="";
+                    });
+                    break;
+                case 'auth/wrong-password':
+                    var alertPopup = $ionicPopup.alert({
+                        title: '發生錯誤',
+                        template: '密碼錯誤，如忘記密碼請點選下方忘記密碼。'
+                    });
+                        alertPopup.then(function(res) {
+                        pwdL.value="";
+                    });
+                    break;
+            }
+        })
+    },false);
+}])
+
+// ----------------------------------------主頁面----------------------------------------
+.controller('page2Ctrl', ['$scope', '$stateParams',
+function ($scope, $stateParams) {
+
+
+}])
+
+// ----------------------------------------設定頁面----------------------------------------
+.controller('page4Ctrl', ['$scope', '$stateParams',
+function ($scope, $stateParams) {
+
+
+}])
---
+++ js/routes.js
@@ -0,0 +1,47 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('tabsController.page2', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/page2.html',
+        controller: 'page2Ctrl'
+      }
+    }
+  })
+
+  .state('tabsController.page4', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/page4.html',
+        controller: 'page4Ctrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('page5', {
+    url: '/page5',
+    templateUrl: 'templates/page5.html',
+    controller: 'page5Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/page5')
+
+
+});
